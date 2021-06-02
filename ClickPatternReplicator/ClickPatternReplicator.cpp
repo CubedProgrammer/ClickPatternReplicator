5726ba02d04cad647d27d8666c1c80ab78d9bc75
CubedProgrammer <47759847+CubedProgrammer@users.noreply.github.com>
2021-06-02 19:36:23 -0400

    Added record command.

--- ClickPatternReplicator/ClickPatternReplicator.cpp
+++ ClickPatternReplicator/ClickPatternReplicator.cpp
@@ -4,12 +4,16 @@
 int main(int argl, char *argv[])
 {
 	std::string cmd;
-	std::vector<std::pair<unsigned, unsigned>>pattern;
+	std::vector<struct Click>pattern;
 	std::cin >> cmd;
+	struct Click* clicks;
 	while (cmd != "exit")
 	{
 		if (cmd == "record")
 		{
+			RecordPattern(&clicks);
+			for (size_t i = 0; clicks[i].button != 0; i++)
+				pattern.push_back(clicks[i]);
 		}
 		std::cin >> cmd;
 	}
