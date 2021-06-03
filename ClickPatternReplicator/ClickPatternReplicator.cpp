e4309ef174c93365a2cb4c2b88de63ce10c49cb5
CubedProgrammer <47759847+CubedProgrammer@users.noreply.github.com>
2021-06-03 17:44:31 -0400

    Fixed fopen compiler warning.

--- ClickPatternReplicator/ClickPatternReplicator.cpp
+++ ClickPatternReplicator/ClickPatternReplicator.cpp
@@ -1,19 +1,23 @@
 #include <iostream>
 #include <vector>
 #include "RecordPattern.h"
+#include "SaveLoad.h"
 int main(int argl, char *argv[])
 {
 	std::string cmd;
 	std::vector<struct Click>pattern;
 	std::cin >> cmd;
 	struct Click* clicks;
+	std::string tmp;
 	while (cmd != "exit")
 	{
 		if (cmd == "record")
 		{
+			std::cin >> tmp;
 			RecordPattern(&clicks);
 			for (size_t i = 0; clicks[i].button != 0; i++)
 				pattern.push_back(clicks[i]);
+			SaveClickPattern(clicks, tmp.c_str());
 		}
 		std::cin >> cmd;
 	}
