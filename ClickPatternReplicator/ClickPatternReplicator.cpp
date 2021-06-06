0965b940fe0031fdd7d7ac74e9d0f0d6362fe70f
CubedProgrammer <47759847+CubedProgrammer@users.noreply.github.com>
2021-06-06 17:50:22 -0400

    Load command.

--- ClickPatternReplicator/ClickPatternReplicator.cpp
+++ ClickPatternReplicator/ClickPatternReplicator.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 #include <vector>
 #include "RecordPattern.h"
 #include "SaveLoad.h"
@@ -7,8 +8,9 @@ int main(int argl, char *argv[])
 	std::string cmd;
 	std::vector<struct Click>pattern;
 	std::cin >> cmd;
-	struct Click* clicks;
+	struct Click* clicks = nullptr;
 	std::string tmp;
+	int delay;
 	while (cmd != "exit")
 	{
 		if (cmd == "record")
@@ -18,6 +20,19 @@ int main(int argl, char *argv[])
 			for (size_t i = 0; clicks[i].button != 0; i++)
 				pattern.push_back(clicks[i]);
 			SaveClickPattern(clicks, tmp.c_str());
+		}
+		else if (cmd == "load")
+		{
+			std::cin >> tmp;
+			LoadClickPattern(&clicks, tmp.c_str());
+			for (size_t i = 0; clicks[i].button != 0; i++)
+				pattern.push_back(clicks[i]);
+		}
+		else if (cmd == "play")
+		{
+			std::cin >> tmp;
+			delay = std::stoi(tmp);
+			for (size_t i = 0; clicks[i].button != 0; i++);
 		}
 		std::cin >> cmd;
 	}
